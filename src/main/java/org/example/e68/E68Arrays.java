493f69f4af4b3e6709ce94f9aade972c8238e3b9
Bezhan Omari <bezhanomari@Bezhans-MacBook-Air.local>
2024-07-30 13:07:46 -0400

    Merge remote-tracking branch 'origin/master'

--- src/main/java/org/example/e68/E68Arrays.java
+++ src/main/java/org/example/e68/E68Arrays.java
@@ -3,13 +3,14 @@ package org.example.e68;
 public class E68Arrays {
     public static void main(String[] args) {
         // Create an array of characters and store the values 's', 'a', 'y', 'b', 'n', 'c', 't', 'd', 'a', 'e', 'x'
-
+        char [] characters={'s', 'a', 'y', 'b', 'n', 'c', 't', 'd', 'a', 'e', 'x'};
 
         // Print the characters at the indices that form the word "syntax"
+        for (int i = 0; i < characters.length; i++) {
 
-
-
-
-
+            if (i%2==0){
+                System.out.print(characters[i]);
+            }
+        }
     }
 }
--- src/main/java/org/example/e70/E70Arrays.java
+++ src/main/java/org/example/e70/E70Arrays.java
@@ -3,13 +3,19 @@ package org.example.e70;
 public class E70Arrays {
     public static void main(String[] args) {
         // Create an array of integers with size 11
+        int [] year=new int [11];
 
 
         // Use a for loop to add the years from 2010 to 2020 to the array
+        for (int i=0; i<year.length; i++){
 
+            year[i]= i+2010;
 
+        }
         // Use another for loop to iterate through the array and print each value on a new line
-
+        for (int i=0; i<year.length; i++){
+            System.out.println(year[i]);
+        }
         // Declare and initialize the array of integers with size 11
 
     }
